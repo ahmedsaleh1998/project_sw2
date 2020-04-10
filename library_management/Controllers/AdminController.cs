024c4e30ebdfb5b5533809119d90b0fd4116fc05
Engineer-Hossam <hossan.h.hassan@gmail.com>
2020-04-10 03:59:01 +0200

    Create Admin Home Page

---
+++ library_management/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lib_manage_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult AdminHomePage()
+        {
+            return View();
+        }
+    }
+}
