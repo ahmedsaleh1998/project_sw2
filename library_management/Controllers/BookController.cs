e8e72594cc555da7638e59b9662a577b5c297146
Engineer-Hossam <hossan.h.hassan@gmail.com>
2020-04-13 09:53:38 +0200

    Edit Book4

--- library_management/Controllers/BookController.cs
+++ library_management/Controllers/BookController.cs
@@ -69,8 +69,70 @@ namespace library_management.Controllers
             ViewBag.Category_Id = new SelectList(db.Categories, "Category_Id", "Category_Name", book.Category_Id);
             return View(book);
         }
+        /////////////////////////////////// Edit Book (Get Method) /////////////////////////
+        public ActionResult Edit_Book(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            TempData["imgPath"] = book.Image;
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Author_Id = new SelectList(db.Authors, "Author_Id", "Author_Name", book.Author_Id);
+            ViewBag.Category_Id = new SelectList(db.Categories, "Category_Id", "Category_Name", book.Category_Id);
+            return View(book);
+        }
 
-        
+        /////////////////////////////////// Edit Book (Post Method) /////////////////////////
+        [HttpPost]
+        public ActionResult Edit_Book(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (book.ImageFile != null)
+                {
+                    string FileName = Path.GetFileNameWithoutExtension(book.ImageFile.FileName);
+                    string Extention = Path.GetExtension(book.ImageFile.FileName);
+                    FileName = FileName + Extention;
+                    book.Image = "~/Image/" + FileName;
+                    FileName = Path.Combine(Server.MapPath("~/Image/"), FileName);
+
+                    if (Extention.ToLower() == ".jpg" || Extention.ToLower() == ".jpeg" || Extention.ToLower() == ".png")
+                    {
+                        book.ImageFile.SaveAs(FileName);
+                        db.Entry(book).State = EntityState.Modified;
+                        db.SaveChanges();
+                        string old_image = Request.MapPath(TempData["imgPath"].ToString());
+                        if (System.IO.File.Exists(old_image))
+                        {
+                            System.IO.File.Delete(old_image);
+                        }
+                        return RedirectToAction("Index_Book");
+                    }
+                    else
+                    {
+                        ViewBag.msg = "Invaild File Type";
+                    }
+
+                }
+                else
+                {
+                    book.Image = TempData["imgPath"].ToString();
+                    db.Entry(book).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index_Book");
+                }
+            }
+            ViewBag.Author_Id = new SelectList(db.Authors, "Author_Id", "Author_Name", book.Author_Id);
+            ViewBag.Category_Id = new SelectList(db.Categories, "Category_Id", "Category_Name", book.Category_Id);
+            return View(book);
+        }
+
+
 
     }
 }
